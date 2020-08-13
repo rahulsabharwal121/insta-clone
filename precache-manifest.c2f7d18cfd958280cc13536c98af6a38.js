5ebb088b7135ff9eca6b53a1de9fce979c731994
Rahul <sabharwalrahul2000@gmail.com>
2020-08-13 12:39:08 +0530

    Updates

---
+++ precache-manifest.c2f7d18cfd958280cc13536c98af6a38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7acd544ce68d97ffd7d69b479cdcdcbf",
+    "url": "/insta-clone/index.html"
+  },
+  {
+    "revision": "03e1cac1010baf7e5a53",
+    "url": "/insta-clone/static/css/main.758ef526.chunk.css"
+  },
+  {
+    "revision": "239c5f516b734d044218",
+    "url": "/insta-clone/static/js/2.3f0be624.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/insta-clone/static/js/2.3f0be624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e1cac1010baf7e5a53",
+    "url": "/insta-clone/static/js/main.cd671028.chunk.js"
+  },
+  {
+    "revision": "c08a09f734ecd40718db",
+    "url": "/insta-clone/static/js/runtime-main.062a3b0b.js"
+  }
+]);
